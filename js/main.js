1ba6f3697aebfd4cf7567343c17ce567e036f694
Igor Roque <igorlinux95@gmail.com>
2015-04-08 15:49:34 -0300

    Terminando a função anônima do objeto Paciente

--- js/main.js
+++ js/main.js
@@ -1,39 +1,23 @@
-function calcularImc() {
-		// VARIÁVEL PARA PEGAR OS PACIENTES QUE ESTÃO NOS TR'S DO HTML
-	var trPaciente = document.getElementsByClassName('paciente');
-	
-	// LOOP PARA PASSAR POR TODOS OS PACIENTES
-	for(var contador = 0; contador <= trPaciente.length; contador++) {
-	
-		// VARIÁVEL PARA PEGAR O PACIENTE SELECIONADO PELA ARRAY
-		var pacientes = trPaciente[contador];
-	
-		// VARIÁVEIS PARA PEGAR OS VALORES DOS TD'S DO PACIENTE SELECIONADO PELA ARRAY
-		var tdNome = pacientes.getElementsByClassName('info-nome')[0];
-		var tdPeso = pacientes.getElementsByClassName('info-peso')[0];
-		var tdAltura = pacientes.getElementsByClassName('info-altura')[0];
-		var tdImc = pacientes.getElementsByClassName('info-imc')[0];
-	
-		// OBJETO QUE CONTÉM AS INFORMAÇÕES DO PACIENTE
-		var paciente = {
-			nome: tdNome.textContent,
-			peso: tdPeso.textContent,
-			altura: tdAltura.textContent
+var TrsPaciente = document.getElementsByClassName('paciente');
+
+for (var contador = 0; contador < TrsPaciente.length; contador++) {
+
+	var pacienteTr = TrsPaciente[contador];
+
+	var tdNome = pacienteTr.getElementsByClassName('info-nome')[0];
+	var tdPeso = pacienteTr.getElementsByClassName('info-peso')[0];
+	var tdAltura = pacienteTr.getElementsByClassName('info-altura')[0];
+	var tdImc = pacienteTr.getElementsByClassName('info-imc')[0];
+
+	var paciente = {
+		nome : tdNome.textContent,
+		peso : tdPeso.textContent,
+		altura : tdAltura.textContent,
+		pegaImc : function() {
+			var imc = this.peso / (this.altura * this.altura);
+			return imc;
 		}
-	
-		// PEGAR O VALOR DO IMC DO PACIENTE SELECIONADO E GUARDAR
-		var imc = paciente.peso / (paciente.altura * paciente.altura);
-		
-		// ATRIBUIR O IMC CALCULADO AO CAMPO DO HTML
-		tdImc.textContent = imc;
 	}
+
+	tdImc.textContent = paciente.pegaImc();
 }
-
-var btn_calcular = document.getElementById('calcular-imc');
-
-btn_calcular.addEventListener('click', calcularImc);
-
-
-var inptNome = document.getElementById('campo-nome').value;
-var inptPeso = document.getElementById('campo-peso').value;
-var inptAltura = document.getElementById('campo-altura').value
